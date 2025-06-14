d50c3e916cb30394576790b1d26a0fa62c5740ac
X1 <corloss@web.de>
2025-06-14 13:19:56 +0200

    state machine encoder

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,26 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+// Pin definitions
+#define ENCODER_PIN_A     10
+#define ENCODER_PIN_B     9
+#define ENCODER_BTN_PIN   11
+#define BTN_NEXT_SCREEN   8
+#define BTN_PREV_SCREEN   12
+#define VACUUM_SENSOR_PIN A6
+
 // OLED setup
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Vacuum sensor
-#define VACUUM_SENSOR_PIN A6
-
-// Rotary encoder pins
-#define ENCODER_BTN_PIN 11
-#define ENCODER_PIN_A   10 // Clockwise
-#define ENCODER_PIN_B   9  // Counterclockwise
-
-// Screen navigation buttons
-#define BTN_NEXT_SCREEN 8
-#define BTN_PREV_SCREEN 12
-
-long target_mbar      = 0; // Target pressure in mbar
-const long min_target = 0;
-const long max_target = 1200;
-
-int lastEncoderA    = LOW;
-int lastButtonState = HIGH;
-bool stepIsTen      = true; // true = 10 mbar, false = 1 mbar
-
-int lastBtnNextState = HIGH;
-int lastBtnPrevState = HIGH;
-
-const unsigned long updateInterval = 500; // ms
-unsigned long lastUpdate           = 0;
+// Globals
+const int min_target = 0;
+const int max_target = 1200;
+int target_mbar      = 0;
+int calib_offset     = 0;
+bool stepIsTen       = true;
 
 enum Screen
 {
@@ -42,10 +31,24 @@ enum Screen
 };
 Screen currentScreen = MAIN;
 
+// Encoder state
+int lastA                 = HIGH;
+unsigned long lastEncMove = 0;
+#define ENCODER_DEBOUNCE_MS 5
+
+// Encoder state for state machine
+int8_t encoderPos    = 0;
+int8_t encoderDelta  = 0;
+uint8_t lastEncState = 0;
+
+const int8_t enc_states[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+
+// Timing
+unsigned long lastUpdate           = 0;
+const unsigned long updateInterval = 100; // ms
+
 void setup()
 {
-    Serial.begin(9600);
-
     pinMode(ENCODER_PIN_A, INPUT_PULLUP);
     pinMode(ENCODER_PIN_B, INPUT_PULLUP);
     pinMode(ENCODER_BTN_PIN, INPUT_PULLUP);
@@ -54,7 +57,6 @@ void setup()
 
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
     {
-        Serial.println(F("SSD1306 allocation failed"));
         for (;;)
             ;
     }
@@ -62,143 +64,128 @@ void setup()
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
-    display.println("Vacuum Sensor Test");
+    display.println("Vacuum Sensor Ready");
     display.display();
     delay(1000);
 
-    lastEncoderA     = digitalRead(ENCODER_PIN_A);
-    lastButtonState  = digitalRead(ENCODER_BTN_PIN);
-    lastBtnNextState = digitalRead(BTN_NEXT_SCREEN);
-    lastBtnPrevState = digitalRead(BTN_PREV_SCREEN);
+    lastA = digitalRead(ENCODER_PIN_A);
+
+    // Initialize encoder state
+    lastEncState = 0;
+    if (digitalRead(ENCODER_PIN_A)) lastEncState |= 2;
+    if (digitalRead(ENCODER_PIN_B)) lastEncState |= 1;
 }
 
-void readEncoder()
+void handleEncoder()
 {
-    int encoderA = digitalRead(ENCODER_PIN_A);
-    int encoderB = digitalRead(ENCODER_PIN_B);
+    int a             = digitalRead(ENCODER_PIN_A);
+    int b             = digitalRead(ENCODER_PIN_B);
+    unsigned long now = millis();
 
-    long step = stepIsTen ? 10 : 1;
-
-    if (encoderA != lastEncoderA)
+    if (a != lastA && a == HIGH && (now - lastEncMove) > ENCODER_DEBOUNCE_MS)
     {
-        if (encoderA == HIGH)
+        int step = stepIsTen ? 10 : 1;
+        if (b == LOW)
         {
-            if (encoderB == LOW)
-            {
-                target_mbar += step; // Clockwise
-            }
-            else
-            {
-                target_mbar -= step; // Counterclockwise
-            }
-            if (target_mbar < min_target) target_mbar = min_target;
-            if (target_mbar > max_target) target_mbar = max_target;
+            target_mbar += step; // Clockwise
         }
-        lastEncoderA = encoderA;
+        else
+        {
+            target_mbar -= step; // Counterclockwise
+        }
+        if (target_mbar < min_target) target_mbar = min_target;
+        if (target_mbar > max_target) target_mbar = max_target;
+        lastEncMove = now;
     }
+    lastA = a;
 }
 
-void readEncoderButton()
+void handleEncoderButton()
 {
-    int buttonState = digitalRead(ENCODER_BTN_PIN);
-    if (lastButtonState == HIGH && buttonState == LOW)
+    static int lastBtn = HIGH;
+    int btn            = digitalRead(ENCODER_BTN_PIN);
+    if (lastBtn == HIGH && btn == LOW)
     {
         stepIsTen = !stepIsTen;
     }
-    lastButtonState = buttonState;
+    lastBtn = btn;
 }
 
-void readScreenButtons()
+void handleEncoderStateMachine()
 {
-    int btnNextState = digitalRead(BTN_NEXT_SCREEN);
-    int btnPrevState = digitalRead(BTN_PREV_SCREEN);
+    // Read both pins and combine into 2-bit value
+    uint8_t enc = 0;
+    if (digitalRead(ENCODER_PIN_A)) enc |= 2;
+    if (digitalRead(ENCODER_PIN_B)) enc |= 1;
 
-    // D8 pressed: go to second screen
-    if (lastBtnNextState == HIGH && btnNextState == LOW)
+    // Combine last state and current state into 4-bit index
+    uint8_t idx = (lastEncState << 2) | enc;
+    encoderDelta += enc_states[idx];
+    lastEncState = enc;
+
+    // Only update value if a full detent (4 steps) is reached
+    if (encoderDelta >= 4)
     {
-        currentScreen = SECOND;
+        int step = stepIsTen ? 10 : 1;
+        target_mbar += step;
+        if (target_mbar > max_target) target_mbar = max_target;
+        encoderDelta = 0;
     }
-    // D12 pressed: go back to main screen
-    if (lastBtnPrevState == HIGH && btnPrevState == LOW)
+    else if (encoderDelta <= -4)
     {
-        currentScreen = MAIN;
+        int step = stepIsTen ? 10 : 1;
+        target_mbar -= step;
+        if (target_mbar < min_target) target_mbar = min_target;
+        encoderDelta = 0;
     }
-    lastBtnNextState = btnNextState;
-    lastBtnPrevState = btnPrevState;
 }
 
-void drawMainScreen(float vacuum_mbar, float voltage)
+void drawMainScreen(int vacuum_mbar, int voltage_mv, int adc_samples)
 {
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextSize(1);
     display.print("Vacuum: ");
-    display.print(vacuum_mbar, 1);
+    display.print(vacuum_mbar - calib_offset);
     display.println(" mbar");
-    display.print("Voltage: ");
-    display.print(voltage, 3);
-    display.println(" V");
+    display.print("Sensor: ");
+    display.print(voltage_mv);
+    display.println(" mV");
     display.print("Target: ");
     display.print(target_mbar);
     display.println(" mbar");
     display.print("Step: ");
     display.print(stepIsTen ? 10 : 1);
     display.println(" mbar");
-    display.display();
-}
-
-void drawSecondScreen()
-{
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.setTextSize(2);
-    display.println("Second");
-    display.setTextSize(1);
-    display.println("Screen");
-    display.println("Placeholder");
+    display.print("ADC samples: ");
+    display.println(adc_samples);
     display.display();
 }
 
 void loop()
 {
-    readEncoder();
-    readEncoderButton();
-    readScreenButtons();
+    static long adcSum  = 0;
+    static int adcCount = 0;
+
+    // Take one ADC sample every loop
+    adcSum += analogRead(VACUUM_SENSOR_PIN);
+    adcCount++;
+
+    handleEncoderStateMachine();
+    handleEncoderButton();
 
     unsigned long now = millis();
     if (now - lastUpdate >= updateInterval)
     {
         lastUpdate = now;
 
-        if (currentScreen == MAIN)
-        {
-            // Take 128 samples and average
-            long adcSum = 0;
-            for (int i = 0; i < 128; i++)
-            {
-                adcSum += analogRead(VACUUM_SENSOR_PIN);
-            }
-            float adcValue = adcSum / 128.0;
-            float voltage  = adcValue * (5.0 / 1023.0);
+        int adcValue    = (adcCount > 0) ? (adcSum / adcCount) : 0;
+        int voltage_mv  = (adcValue * 5000L) / 1023;
+        int vacuum_mbar = ((voltage_mv - 500) * 1000L) / (4500 - 500);
 
-            float vacuum_bar  = (voltage - 0.5) * (1.0 / (4.5 - 0.5));
-            float vacuum_mbar = vacuum_bar * 1000.0;
+        drawMainScreen(vacuum_mbar, voltage_mv, adcCount);
 
-            drawMainScreen(vacuum_mbar, voltage);
-
-            // Serial output
-            Serial.print("Vacuum: ");
-            Serial.print(vacuum_mbar, 1);
-            Serial.print(" mbar, Voltage: ");
-            Serial.print(voltage, 3);
-            Serial.print(" V, Target: ");
-            Serial.print(target_mbar);
-            Serial.print(" mbar, Step: ");
-            Serial.println(stepIsTen ? 10 : 1);
-        }
-        else if (currentScreen == SECOND)
-        {
-            drawSecondScreen();
-        }
+        adcSum   = 0;
+        adcCount = 0;
     }
 }
